3e8d6039c883293b273f783b67b2c068d7f62832
Marcio Galli <marciogalli@taboca.local>
2010-04-13 16:32:32 -0700

    default to run execute from the startup

--- xulrunner/base/defaults/preferences/mybrowser-prefs.js
+++ xulrunner/base/defaults/preferences/mybrowser-prefs.js
@@ -1,2 +1,4 @@
 pref("toolkit.defaultChromeURI", "chrome://telasocial/content/telasocial.xul");
 pref("general.useragent.extra.telasocial", "TelaSocial CrowdBrowsing/0.1");
+pref("extensions.tagvisor.auto",true);
+
