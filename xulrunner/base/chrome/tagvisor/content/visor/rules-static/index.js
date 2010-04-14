d1a67f5e7e427953745030f2edd633e47aed657b
Marcio Galli <marciogalli@taboca.local>
2010-04-14 14:23:17 -0700

    fix calendar

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index.js
@@ -1,7 +1,7 @@
  
 		
 		com.taboca.upvisor.registerWidget( null , "com/icmc/gfxfullvideo/gfxless.js","container");
-		com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar-id.js","calendar");
+		com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar.js","calendar");
 
 		com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_topright");
                 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-eventos/twitter.js","twitter", "area_midleft");
