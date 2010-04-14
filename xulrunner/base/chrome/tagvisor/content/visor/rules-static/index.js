655b614d817dbdedb64f19a11bc06dabbd1f6d5c
Marcio Galli <marciogalli@taboca.local>
2010-04-14 14:28:55 -0700

    fix simple

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index.js
@@ -4,8 +4,8 @@
 		com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar.js","calendar");
 
 		com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_topright");
-                com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-eventos/twitter.js","twitter", "area_midleft");
                 com.taboca.upvisor.registerWidget( ".container" , "com/icmc/magna/grade.js","grade", "area_midright");
+                com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-eventos/twitter.js","twitter", "area_midleft");
 
 
 		com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".calendar", "start"); } ); 
