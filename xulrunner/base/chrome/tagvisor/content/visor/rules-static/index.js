c760359d4c5c82f16d090101812ef675d33fd312
Marcio Galli <marciogalli@taboca.local>
2010-04-14 18:08:39 -0700

    just slashdot

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index.js
@@ -4,7 +4,7 @@
 		com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar.js","calendar");
 
 		com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_topright");
-                com.taboca.upvisor.registerWidget( ".container" , "com/icmc/magna/grade.js","grade", "area_midleft");
+//                com.taboca.upvisor.registerWidget( ".container" , "com/icmc/magna/grade.js","grade", "area_midleft");
                 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-slash/twitter.js","twitter", "area_midright");
 
 
