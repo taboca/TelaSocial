2596218a240e3d9f2aa90a076c09f76d6e706319
Marcio Galli <marciogalli@18743205249.3G.telemar.net.br>
2010-04-22 08:24:22 -0700

    Usp main

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-icmc-uspmain.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-icmc-uspmain.js
@@ -2,7 +2,7 @@
 		com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar-icmc.js","calendar");
 		com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_topright");
                 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-eventos/twitter.js","twitter", "area_midleft");
-                com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-icmcoutros/twitter.js","twitterslash", "area_midright");
+                com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-uspmain/twitter.js","twitterslash", "area_midright");
 		com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".calendar", "start"); } ); 
 
 
