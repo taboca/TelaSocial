a00ca3e8b2366887fd0ccfa7a43c5db1707dca36
Marcio Galli <marciogalli@taboca.local>
2010-04-27 18:26:40 -0700

    fix to

--- xulrunner/base/chrome/tagvisor/content/visor/com/icmc/gfxless-shift/gfxless.js
+++ xulrunner/base/chrome/tagvisor/content/visor/com/icmc/gfxless-shift/gfxless.js
@@ -180,7 +180,7 @@ var gfx =  {
 </td><td valign='top'>
 
 <div id='right' style='width:1920px'>
-<span style='color:black;font-size:30px;font-weight:bold;' >http:\/\/www4.usp.br\/index.php\/noticias?format=feed&type=rss</span>
+<span style='color:black;font-size:30px;font-weight:bold;' >http:\/\/www4.usp.br\/index.php\/noticias?format=feed&amp;type=rss</span>
 <div class='orange2' style='margin-top:0em;' id='area_midright'>
 </div>
 </div>
