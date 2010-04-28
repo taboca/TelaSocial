0c586961afe7354c4f335d92642996a6af08b30f
Marcio Galli <marciogalli@taboca.local>
2010-04-27 18:46:40 -0700

    fix to

--- xulrunner/base/chrome/tagvisor/content/visor/com/taboca/twitter-uspmain/twitter.js
+++ xulrunner/base/chrome/tagvisor/content/visor/com/taboca/twitter-uspmain/twitter.js
@@ -95,7 +95,7 @@ var twitter =  {
 		var i;
 		for (i = 0; i < result.feed.entries.length; i++) {
 			if (result.feed.entries[i]) {
-				this.tweetQueue.push(result.feed.entries[i].title + ' <span class="tweetauthor"> (por USP.br RSS)</span>');
+				this.tweetQueue.push( '<img width="64" src="http://go.bath.ac.uk/qr/download?DATA='+result.feed.entries[i].link+'" style="margin-right:15px; margin-bottom:10px; " align="left" />' +   result.feed.entries[i].title + ' <span class="tweetauthor"> (por USP.br RSS)</span>');
 			}
 		}
 
