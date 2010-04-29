a85795e0ecf3b0e5f175c93c55b23c08c6fca2a0
Marcio Galli <marciogalli@taboca.local>
2010-04-28 20:43:38 -0700

    fix module name

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-bloco1-icmc.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-bloco1-icmc.js
@@ -2,7 +2,7 @@
 	com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar-shift.js","calendar");
 	com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_topright");
         com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-eventos/twitter.js","twitter", "area_midleft");
-        com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-bloco4/twitter.js","twitterslash", "area_midright");
+        com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-bloco1/twitter.js","twitterslash", "area_midright");
 	com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".calendar", "start"); } ); 
 
 
