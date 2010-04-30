34f65852f175b4abda14943ab69bc8c15d691ab7
Marcio Galli <marciogalli@taboca.local>
2010-04-30 15:23:03 -0700

    clenaup

--- xulrunner/base/chrome/tagvisor/content/visor/com/taboca/fade/fade.js
+++ xulrunner/base/chrome/tagvisor/content/visor/com/taboca/fade/fade.js
@@ -6,6 +6,7 @@ var fade_Widget =  {
         name   : __appName,
         target : __targetName,
         targetId    : __targetId,
+
 	zIndex      :10,
 	refElement: null, 
 
@@ -15,19 +16,14 @@ var fade_Widget =  {
 	fadeCycle    : 0,
 
 	start : function () { 
-
 		this.feed = new this._service_google.feeds.Feed('http://api.flickr.com/services/feeds/photos_public.gne?tags=flowers');
 		this.feed.setNumEntries(20);
 		this.feed.setResultFormat(this._service_google.feeds.Feed.XML_FORMAT);
-
 		this.element = this._coreDoc.createElement('div');
 		this.picQueue = new Array();
-
 		this._coreDoc.getElementById(this._getId()).appendChild(this.element);
-
 		this.refElement = this._coreDoc.createElement("div");
 		this.element.appendChild(this.refElement);
-
 		this.popPic();
 	} ,
 
@@ -39,49 +35,32 @@ var fade_Widget =  {
 		if (this.picQueue.length == 0) { 
 			this.feed.load(this.__feedUpdated.bind(this));
 		} else { 
-
 		var k = this._coreDoc.createElement('div');
-
 		k.style.opacity=0.01;
-
 		k.style.position="absolute"
-
 		k.style.zIndex=this.zIndex;
 		this.zIndex++;
 		k.className = 'photo';
 		var t = this.picQueue.pop();
-
 		t = "<img id='fadeimage"+this.imageNumber+"' src='"+t+"' >";
-
-		
-
 		k.innerHTML = t;
-
 		this.lastInserted = k;
-		//this.element.appendChild(k);
-
 		var oldOldChild=null;
 		if(this.oldChild)  {
 			oldOldChild=this.oldChild;	
 		} 
 		this.oldChild = this.element.firstChild;
-
 		this.element.insertBefore(k, this.element.firstChild);
-
 		if(oldOldChild) { 
 			this.element.removeChild(oldOldChild);
 		} 
-
 		these = this;
 		this._coreDoc.getElementById("fadeimage"+this.imageNumber).onload = function () { these.imageLoaded() };
-
-
 		return true;
-
 		} 
 	},
-	imageLoaded : function() { 
 
+	imageLoaded : function() { 
 		var currImage =  this._coreDoc.getElementById("fadeimage"+this.imageNumber);
 		var x= parseInt(currImage.width); 
 		var y= parseInt(currImage.height); 
@@ -91,9 +70,7 @@ var fade_Widget =  {
 
 	} ,
 
-
 	kickFadeIn : function () { 
-
 		if(this.fadeCycle<=1) { 
 			this.fadeCycle+=0.5;
 			this.lastInserted.style.opacity= this.fadeCycle;
@@ -101,14 +78,11 @@ var fade_Widget =  {
                 	timer.setTimeout( function () { scopedThis.kickFadeIn() }, 500);
 
 		} else { 
-
-			
 			this.fadeCycle=0;
 			var scopedThis = this;
                 	timer.setTimeout( function () { scopedThis.popPic() }, 500);
 		} 
 	} ,
-
 
 	updateFeed : function() {
 		if (! this.popPic()) {
@@ -116,15 +90,11 @@ var fade_Widget =  {
 	},
 
 	__feedUpdated : function(result) {
-
 		if (result.error ) {
 			return;
 		}
-
 		var doc = result.xmlDocument;
-
 		var links = doc.getElementsByTagName("link");
-
 		var i;
 		for (i = 0; i < links.length; i++) {
 			if (links[i].getAttribute("rel") == "enclosure") { 
@@ -133,9 +103,7 @@ var fade_Widget =  {
 				this.picQueue.push(src);
 			} 
 		}
-
 		this.popPic();
-
 	}
 }
 
