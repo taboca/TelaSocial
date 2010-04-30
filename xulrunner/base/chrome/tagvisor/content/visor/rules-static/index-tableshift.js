c8a3e48235a54282e24f585e6865fb2884b3adf9
Marcio Galli <marciogalli@taboca.local>
2010-04-30 14:17:34 -0700

    rules to drumbeat panel

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-tableshift.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-tableshift.js
@@ -2,7 +2,7 @@
 	com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar-tableshift.js","calendar");
 	com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_panel1");
         com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-eventos/twitter.js","twitter", "area_panel2");
-        com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-bloco1/twitter.js","twitterslash", "area_panel3");
+        com.taboca.upvisor.registerWidget( ".container" , "com/taboca/flickr/flickr.js","twitterslash", "area_panel3");
 	com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".calendar", "start"); } ); 
 
 
