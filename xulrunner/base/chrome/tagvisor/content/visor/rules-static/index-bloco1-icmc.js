ee8632dfc278783eaaa64140cc109b9e06aa767f
Marcio Galli <marciogalli@taboca.local>
2010-05-04 21:57:44 -0700

    fix calendar

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-bloco1-icmc.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-bloco1-icmc.js
@@ -2,6 +2,7 @@
 	com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar-shift.js","calendar");
 	com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_topright");
         com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-eventos/twitter.js","twitter", "area_midleft");
+        com.taboca.upvisor.registerWidget( ".container" , "com/taboca/typing/typing.js","uspmain", "area_bottomleft");
         com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-bloco1/twitter.js","twitterslash", "area_midright");
 	com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".calendar", "start"); } ); 
 
