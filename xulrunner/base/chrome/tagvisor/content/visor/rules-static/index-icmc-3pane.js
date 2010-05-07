60f274c8606c74e3c20235636752a45686ba8bc9
Marcio Galli <marciogalli@taboca.local>
2010-05-07 13:47:27 -0700

    fix rules

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-icmc-3pane.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-icmc-3pane.js
@@ -1,4 +1,4 @@
-com.taboca.upvisor.registerWidget( null , "com/icmc/gfxless-3pane/gfxless.js","container");
+com.taboca.upvisor.registerWidget( null , "com/icmc/3pane/gfxless.js","container");
 com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar-shift.js","calendar");
 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_panel1");
 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-eventos/twitter.js","twitter", "area_panel2");
