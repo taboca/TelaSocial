2ffdea555825703384f0a75f2f395548265e83e1
Marcio Galli <marciogalli@taboca.local>
2010-05-07 12:54:51 -0700

    New rules to 3 panel

---
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-icmc-3pane.js
@@ -0,0 +1,14 @@
+com.taboca.upvisor.registerWidget( null , "com/icmc/gfxless-3pane/gfxless.js","container");
+com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar-shift.js","calendar");
+com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_panel1");
+com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-eventos/twitter.js","twitter", "area_panel2);
+com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-bloco1/twitter.js","twitterslash", "area_panel3");
+com.taboca.upvisor.registerWidget( ".container" , "com/taboca/typing/typing.js","uspmain", "area_panelbottom");
+
+com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".calendar", "start"); } ); 
+
+
+
+
+
+
