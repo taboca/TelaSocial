0e929b1ee2eba31abc31c07917c95b4a57ca03c3
Marcio Galli <marciogalli@taboca.local>
2010-05-07 13:58:40 -0700

    fix name

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-icmc-3pane.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-icmc-3pane.js
@@ -3,7 +3,7 @@ com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar-icmc-3pa
 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_panel1");
 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-eventos/twitter.js","twitter", "area_panel2");
 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-bloco1/twitter.js","twitterslash", "area_panel3");
-com.taboca.upvisor.registerWidget( ".container" , "com/taboca/typing/typing.js","uspmain", "area_panelbottom");
+com.taboca.upvisor.registerWidget( ".container" , "com/taboca/typing/typing.js","uspmain", "area_bottom");
 
 com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".calendar", "start"); } ); 
 
