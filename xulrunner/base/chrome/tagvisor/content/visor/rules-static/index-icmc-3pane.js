38290e8ef83889ccde102dcf03c8b7c891844a64
Marcio Galli <marciogalli@taboca.local>
2010-05-07 17:24:53 -0700

    Layout

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-icmc-3pane.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-icmc-3pane.js
@@ -1,6 +1,6 @@
 com.taboca.upvisor.registerWidget( null , "com/icmc/3pane/gfxless.js","container");
 com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar-icmc-3pane.js","calendar");
-com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_panel1");
+com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_bottomright");
 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-eventos/twitter.js","twitter", "area_panel2");
 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-bloco1/twitter.js","twitterslash", "area_panel3");
 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/typing/typing.js","uspmain", "area_bottom");
