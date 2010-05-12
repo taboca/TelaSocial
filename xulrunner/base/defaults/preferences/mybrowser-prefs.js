39043b283d5440abf59900313448e1ff90375784
Marcio Galli <marciogalli@taboca.local>
2010-05-12 16:17:12 -0700

    fix ubuntu dpi layout for CCW mode tests

--- xulrunner/base/defaults/preferences/mybrowser-prefs.js
+++ xulrunner/base/defaults/preferences/mybrowser-prefs.js
@@ -1,4 +1,6 @@
 pref("toolkit.defaultChromeURI", "chrome://telasocial/content/telasocial.xul");
 pref("general.useragent.extra.telasocial", "TelaSocial CrowdBrowsing/0.1");
 pref("extensions.tagvisor.auto",true);
+// for CCW mode we fixed ubuntu with pref("layout.css.dpi", 72);
+
 
