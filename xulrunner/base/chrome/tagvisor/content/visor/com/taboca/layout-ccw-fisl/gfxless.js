364bee764953bb37d284f7283c16a49dda524083
Marcio Galli <marciogalli@taboca.local>
2010-06-14 21:07:16 -0700

    layout prototype FISL proposal

--- xulrunner/base/chrome/tagvisor/content/visor/com/taboca/layout-ccw-fisl/gfxless.js
+++ xulrunner/base/chrome/tagvisor/content/visor/com/taboca/layout-ccw-fisl/gfxless.js
@@ -14,26 +14,27 @@ var gfx =  {
 	html { padding:0; margin:0; overflow:hidden; font-family: verdana, arial, sans-serif } 
 
 	body {
-		background-color:rgb(253,181,37); margin:0; padding:0;
+		background-color:rgb(255,255,255); margin:0; padding:0;
 	}
 
 	table {
-		width:100%;
 		margin:0;
 		z-index:1000;
 		position:absolute;
 	}
 
 	table td {
-		border:10px solid black;
+		border:0px solid white;
 	}
 
 	.panel {
+		width:1060px;
 	}
 
 	.tab {
                 font-size:20px;
-		width:620px;
+		width:350px;
+		padding:1em;
                 font-weight:bold;
         }
 
@@ -43,10 +44,11 @@ var gfx =  {
 
 	#area_bottomright { 
 		text-align:center;
+		width:360px;
 		background-color:rgb(70,70,70);	
 	} 
 
-        .cor_orange { background-color:rgb(253,181,37);; }
+        .cor_orange { background-color:rgb(255,255,255);; }
         .cor_lightblue  { background-color:rgb(100,197,210); }
         .cor_tab3   {
 		background-color:rgb(14,148,171);	
@@ -59,10 +61,10 @@ var gfx =  {
                 width:45px;
                 -moz-border-radius:25px;
                 height:45px;
-                background-color:black;
-                z-index:1;
+                background-color:lightblue;
+                z-index:1111;
                 position:absolute;
-                top:44px;
+                top:320px;
                 left:0px;
         }
 
@@ -73,7 +75,7 @@ var gfx =  {
 
 	.panel { 
 		display:none;
-		height:700px;
+		height:1350px;
 		overflow:hidden;
 	} 
 
@@ -91,6 +93,7 @@ var gfx =  {
 }
 
 	#area_bottom {
+		padding:1em;
                 height:230px;
                 background-color:rgb(30,30,30);
     		font-family:Kaffeesatz,Verdana, Arial, Helvetica, sans-serif;
@@ -108,7 +111,7 @@ var gfx =  {
         kick: function () {
 		this.movepos+=5;
                 this._coreDoc.getElementById("pointer").style.left=this.movepos+"px";
-		if(this.movepos > 1260 && this.movepos < 1950 ) { 
+		if(this.movepos > 700 && this.movepos < 1080 ) { 
                         this._coreDoc.getElementById("tab1").className="cor_orange";
                         this._coreDoc.getElementById("tab2").className="cor_lightblue";
                         this._coreDoc.getElementById("tab3").className="transp";
@@ -117,7 +120,7 @@ var gfx =  {
                         this._coreDoc.getElementById("area_panel2").style.display="none";
                         this._coreDoc.getElementById("area_panel1").style.display="none";
 		} 
-		if(this.movepos > 630 && this.movepos < 1260) { 
+		if(this.movepos > 350 && this.movepos < 700) { 
                         this._coreDoc.getElementById("tab1").className="cor_orange";
                         this._coreDoc.getElementById("tab2").className="transp";
                         this._coreDoc.getElementById("tab3").className="cor_tab3";
@@ -126,16 +129,17 @@ var gfx =  {
                         this._coreDoc.getElementById("area_panel1").style.display="none";
                         this._coreDoc.getElementById("area_panel3").style.display="none";
 		} 
-		if(this.movepos > 0  && this.movepos < 630) { 
+		if(this.movepos > 0  && this.movepos < 350) { 
                         this._coreDoc.getElementById("tab1").className="transp";
                         this._coreDoc.getElementById("tab2").className="cor_lightblue";
                         this._coreDoc.getElementById("tab3").className="cor_tab3";
 			this._coreDoc.body.style.backgroundColor="rgb(253,181,37)";
+			this._coreDoc.body.style.backgroundColor="rgb(255,255,255)";
                         this._coreDoc.getElementById("area_panel1").style.display="block";
                         this._coreDoc.getElementById("area_panel2").style.display="none";
                         this._coreDoc.getElementById("area_panel3").style.display="none";
 		} 
-		if(this.movepos > 1950 ) { 
+		if(this.movepos > 1080 ) { 
 
 			this.movepos = 0
 
@@ -151,49 +155,36 @@ var gfx =  {
 <div id="frame">
 <div id='pointer'>
 </div>
-<table cellpadding="10" cellspacing="0" height="100%">
+<table cellpadding="0" cellspacing="0" height="1920">
+<tr>
+<td colspan="3" style="padding:0" align="left" valign="bottom" >
+<img src="http://softwarelivre.org/designs/themes/fisl11/imgs/header.jpg" width="1074" style="margin:0" height="270"/>
+</td>
+</tr>
 <tr style="height:50px">
 <td class='cor_orange' id='tab1'>
 <div class='tab'>
-Telas ICMC (0.2)
+TelaSocial
 </div>
 </td>
 <td class='cor_lightblue' id='tab2'>
 <div class='tab'>
-Palestras ICMC
+Notícias FISL
 </div>
 </td>
 <td class='cor_tab3' id='tab3'>
 <div class='tab'>
-#bloco1icmc (twitter)
+Twitter#fisl11
 </div>
 </td>
 </tr>
 <tr>
 <td colspan="3" >
-<div class="panel" id='area_panel1'>
-<h2>Versão 0.2 - alpha - 7 Maio 2010</h2>
-<ul>
-<li>Layout novo tipo Abas - feedback via #bloco1icmc ou mgalli em taboca ponto com </li>
-<li>Linha do tempo nas Abas - ponteiro acima para passar percepção das mudanças e saber qdo vem o próximo painel</li>
-<li>Módulo-widget ( typing): Painel fixo com notícias - abaixo com RSS Usp.br. Em breve com multiplexador de RSS ( noticias de várias fontes )</li>
-<li>Aba de Eventos - com datas - irá mudar para uma visão ~ calendário</li>
-<li>Outros: Cores USP</li>
-<li>Outros: Relógio foi pro canto</li>
-<li>Feedback? blog.telasocial.com ou mgalli em taboca ponto com ou #bloco1icmc</li>
-<li>Ideias?: Módulo-widget do bandejão? .. #bloco1icmc</li>
-</ul>
-<h2>Version 0.2 - alpha - 7th, May, 2010</h2>
-<ul>
-<li>New Layout with Tabs</li>
-<li>Timeline perception - at the top little moving black dot. The use case is based on the NOT case of the bus terminal s
-creens at the São Paulo bus station 'When you are about to see your bus time, the ads shows and you miss your bus'.</li>
-<li>New typing widget in the fixed area at the bottom - using USP.br RSS source for now - soon the RSS multiplexer will g
-et in so we should be able to get news from many sources all together ( pipeline effect );</li>
-<li>ICMC presentation events Tab now has dates ; soon will have a better calendar view; </li>
-<li>Other: USP colors</li>
-<li>ideas? #bloco1icmc at twitter or mgalli at taboca dot com </li>
-</ul>
+<div class="panel" id='area_panel1' style='text-align:center;padding:1em;padding-top:250px'>
+
+
+<h1 style='font-size:140px;'>TelaSocial</h1>
+<img src="http://www.mozilla.org/images/poweredby_400.png" />
 
 
 </div>
