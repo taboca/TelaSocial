87c2381ebc5549ce11910dce77d1d8eca3a7f6d9
Marcio Galli <marciogalli@taboca.local>
2010-06-14 21:26:26 -0700

    fix layout rules

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-ccw-fisl.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-ccw-fisl.js
@@ -1,9 +1,9 @@
-org.fisl.upvisor.registerWidget( null , "org/fisl/layout-vertical/gfxless.js","container");
-org.fisl.upvisor.registerWidget( null , "org/fisl/calendar/calendar-fisl.js","calendar");
-org.fisl.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_bottomright");
-org.fisl.upvisor.registerWidget( ".container" , "org/fisl/twitter-eventos/twitter.js","twitter", "area_panel2");
-org.fisl.upvisor.registerWidget( ".container" , "org/fisl/twitter-postit/twitter.js","twitterslash", "area_panel3");
-org.fisl.upvisor.registerWidget( ".container" , "org/fisl/typing/typing.js","uspmain", "area_bottom");
+com.taboca.upvisor.registerWidget( null , "org/fisl/layout-vertical/gfxless.js","container");
+com.taboca.upvisor.registerWidget( null , "org/fisl/calendar/calendar-fisl.js","calendar");
+com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_bottomright");
+com.taboca.upvisor.registerWidget( ".container" , "org/fisl/twitter-eventos/twitter.js","twitter", "area_panel2");
+com.taboca.upvisor.registerWidget( ".container" , "org/fisl/twitter-postit/twitter.js","twitterslash", "area_panel3");
+com.taboca.upvisor.registerWidget( ".container" , "org/fisl/typing/typing.js","uspmain", "area_bottom");
 
 com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".calendar", "start"); } ); 
 
