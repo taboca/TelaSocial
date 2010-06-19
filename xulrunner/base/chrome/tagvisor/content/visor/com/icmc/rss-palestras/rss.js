ac58f7f44e03286a83067fc0a3f38f2450c0f3a4
Marcio Galli <marciogalli@taboca.local>
2010-06-19 14:24:04 -0700

    rss palestras icmc

---
+++ xulrunner/base/chrome/tagvisor/content/visor/com/icmc/rss-palestras/rss.js
@@ -0,0 +1,103 @@
+c     = require("choreographer");
+timer = require("timer");
+
+var twitter =  {
+	name   : __appName,
+        target : __targetName,
+        targetId : __targetId,
+	crop    : "50",
+	title   : "Twitter 10",
+	feedURL : "http://www.icmc.usp.br/eventos/feed/",
+	feed    : null, 
+	style : <><![CDATA[
+		.tweetdate { 
+			color:white;
+			font-weight:bold;
+			font-size:32px;
+			margin-right:.5em;
+			border-right:6px solid white;
+			padding-right:.5em
+		}
+		.tweetauthor { 
+			color:gray; 
+		} 
+		.tweetpublic { 
+			color:black;
+			font-size:40px;
+			font-weight:bold;
+			-moz-border-radius:12px;
+			background-color: rgba(255,255,255,.5);
+			-moz-box-shadow: rgba(255,255,255,.5) 0 0 10px; 
+			padding:10px;
+			margin-top:5px;
+		} 
+		.twitterPanel { 
+			width:1880px;
+		} 
+	]]></>, 
+	start : function() {
+
+                this.elementStore = this._coreDoc.createElement('div');
+		this.elementStore.setAttribute("id","storetemp");
+		this._coreDoc.getElementById(this._getId()).appendChild(this.elementStore);
+
+		this.element = this._coreDoc.createElement('div');
+
+		this.element.className="twitterPanel";
+		this.element.id = Math.random();
+		this.tweetQueue = new Array();
+
+		var first = this._coreDoc.createElement("div");
+		this.firstId = "firsttwitter";
+		first.id = this.firstId;
+
+		this.tweetRepeated = {};
+		this.element.appendChild(first);
+		this._coreDoc.getElementById(this._getId()).appendChild(this.element);
+		var self = this;
+		timer.setTimeout( function(){self.updateFeed()},10000);
+	},
+	init : function () { 
+	 	var style = this._coreDoc.createElementNS("http://www.w3.org/1999/xhtml", "style");
+		this._coreDoc.getElementById("headtarget").appendChild(style);
+		style.innerHTML=this.style; 
+		//this.feed = new this._service_google.feeds.Feed(this.feedURL);
+		this.feed = this._service_jquery;
+	} ,
+	popTweet : function() {
+		if (this.tweetQueue.length == 0) return false;
+		var t = this.tweetQueue.pop();
+		if (t in this.tweetRepeated) {
+			return;
+		}
+		this.tweetRepeated[t] = true;
+		var k = this._coreDoc.createElement('div');
+		k.className = 'tweetpublic';
+		k.innerHTML = t;
+		this.element.insertBefore(k, this.element.firstChild);
+		return true;
+	},
+
+	updateFeed : function() {
+		if (! this.popTweet()) {
+			var self =this; 
+			this.feed.ajax( { type:"GET", url: this.feedURL, dataType: "xml", success: function (xml) {  self.__feedUpdated(xml) } });
+		}
+		var self = this;
+		timer.setTimeout( function(){self.updateFeed()},10000);
+	},
+	__feedUpdated : function(xml) {
+
+		var self  = this; 
+		this.feed(xml).find('item').each(function(){
+			var pubDate = self.feed(this).find('pubDate').text();
+			var title   = self.feed(this).find('title').text();
+			var link    = self.feed(this).find('link').text();
+	
+			self.tweetQueue.push( '<img width="44" src="http://go.bath.ac.uk/qr/download?DATA='+link+'" style="margin-right:15px; margin-bottom:10px; " align="left" /><span class="tweetdate">' + pubDate + '</span>' + title + ' <span class="tweetauthor">(Eventos ICMC)</span>');
+
+		});
+
+	}
+}
+c.register(twitter);
