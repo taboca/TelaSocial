15d0f955340508fc073b8f2b61c9e67c768d68b3
Marcio Galli <marciogalli@taboca.local>
2010-06-22 18:53:30 -0700

    author

--- xulrunner/base/chrome/tagvisor/content/visor/com/taboca/twitter-openvideo/twitter.js
+++ xulrunner/base/chrome/tagvisor/content/visor/com/taboca/twitter-openvideo/twitter.js
@@ -104,7 +104,7 @@ var twitter =  {
 		var i;
 		for (i = 0; i < result.feed.entries.length; i++) {
 			if (result.feed.entries[i]) {
-				this.tweetQueue.push( '<img width="44" src="http://go.bath.ac.uk/qr/download?DATA='+result.feed.entries[i].link+'" style="margin-right:15px; margin-bottom:10px; " align="left" /><span class="tweetdate">' + result.feed.entries[i].publishedDate + '</span>' + result.feed.entries[i].title + ' <span class="tweetauthor">(Eventos ICMC -' + result.feed.entries[i].author.replace(/ \(.*$/,'') + ')</span>');
+				this.tweetQueue.push( '<img width="44" src="http://go.bath.ac.uk/qr/download?DATA='+result.feed.entries[i].link+'" style="margin-right:15px; margin-bottom:10px; " align="left" /><span class="tweetdate">' + result.feed.entries[i].publishedDate + '</span>' + result.feed.entries[i].title + ' <span class="tweetauthor">' + result.feed.entries[i].author.replace(/ \(.*$/,'') + '</span>');
 			}
 		}
 
