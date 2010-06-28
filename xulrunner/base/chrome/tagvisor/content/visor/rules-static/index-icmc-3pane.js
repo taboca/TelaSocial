42117daa11eec6cdc9317392b9175d1c9f27083c
Marcio Galli <marciogalli@taboca.local>
2010-06-28 11:33:20 -0700

    fix redesocial

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-icmc-3pane.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-icmc-3pane.js
@@ -2,7 +2,7 @@ com.taboca.upvisor.registerWidget( null , "com/icmc/3pane/gfxless.js","container
 com.taboca.upvisor.registerWidget( null , "com/taboca/calendar/calendar-icmc-3pane.js","calendar");
 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/clock/clock.js","clock", "area_bottomright");
 com.taboca.upvisor.registerWidget( ".container" , "com/icmc/rss-palestras/rss.js","twitter", "area_panel2");
-com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-bloco1/twitter.js","twitterslash", "area_panel3");
+com.taboca.upvisor.registerWidget( ".container" , "com/taboca/twitter-bloco1/twitter.js","redesocial", "area_panel3");
 com.taboca.upvisor.registerWidget( ".container" , "com/taboca/typing/typing.js","uspmain", "area_bottom");
 
 com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".calendar", "start"); } ); 
