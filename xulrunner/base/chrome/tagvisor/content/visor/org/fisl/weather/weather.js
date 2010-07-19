4627d86c3d641449c5d795abd4f485aaea27bcf1
Marcio Galli <marciogalli@taboca.local>
2010-07-19 14:58:23 -0700

    fix left

--- xulrunner/base/chrome/tagvisor/content/visor/org/fisl/weather/weather.js
+++ xulrunner/base/chrome/tagvisor/content/visor/org/fisl/weather/weather.js
@@ -23,7 +23,7 @@ var clock_Widget =  {
 
 		var mainDiv      = this._coreDoc.createElement('div');
 		mainDiv.setAttribute("width","320");
-		mainDiv.setAttribute("style","text-align:center;padding:10px;-moz-border-radius:25px; margin-top:0px;margin-left:70px;width:320px;  height:180px;color:black; ; overflow:hidden; ");
+		mainDiv.setAttribute("style","text-align:center;padding:10px;-moz-border-radius:25px; margin-top:0px;margin-left:80px;width:320px;  height:180px;color:black; ; overflow:hidden; ");
 		this._coreDoc.getElementById(this._getId()).appendChild(mainDiv);
 
 		this.elementWeather = this._coreDoc.createElement('div');
