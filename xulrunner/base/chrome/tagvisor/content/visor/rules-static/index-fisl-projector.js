3af00c91f5ed1853c143f8b71d0fac1662807736
Marcio Galli <marciogalli@taboca.local>
2010-07-19 14:52:08 -0700

    new rule

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-fisl-projector.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-fisl-projector.js
@@ -1,7 +1,7 @@
 com.taboca.upvisor.registerWidget( null , "org/fisl/layout-projector/gfxless.js","container");
 com.taboca.upvisor.registerWidget( null , "org/fisl/calendar/calendar-projector.js","calendar");
 //com.taboca.upvisor.registerWidget( ".container" , "org/fisl/twitter-eventos-side/twitter.js","clock", "area_bottomright");
-com.taboca.upvisor.registerWidget( ".container" , "org/fisl/weather/weather.js","weather", "area_topright");
+com.taboca.upvisor.registerWidget( ".container" , "org/fisl/weather/weather.js","weather", "area_topmid");
 com.taboca.upvisor.registerWidget( ".container" , "org/fisl/clock/clock.js","clock", "area_topleft");
 
 com.taboca.upvisor.registerWidget( ".container" , "org/fisl/noticias-fisl/noticias.js","fislnews", "area_panel3");
