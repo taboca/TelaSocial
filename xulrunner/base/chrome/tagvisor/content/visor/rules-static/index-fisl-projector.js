36aac7ff9ce3210f8de4803f59861f06e7130b88
Marcio Galli <marciogalli@taboca.local>
2010-07-20 13:10:08 -0700

    rules change logo with clock

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-fisl-projector.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-fisl-projector.js
@@ -2,7 +2,7 @@ com.taboca.upvisor.registerWidget( null , "org/fisl/layout-projector/gfxless.js"
 com.taboca.upvisor.registerWidget( null , "org/fisl/calendar/calendar-projector.js","calendar");
 //com.taboca.upvisor.registerWidget( ".container" , "org/fisl/twitter-eventos-side/twitter.js","clock", "area_bottomright");
 com.taboca.upvisor.registerWidget( ".container" , "org/fisl/weather/weather.js","weather", "area_topmid");
-com.taboca.upvisor.registerWidget( ".container" , "org/fisl/clock/clock.js","clock", "area_topleft");
+com.taboca.upvisor.registerWidget( ".container" , "org/fisl/clock/clock.js","clock", "area_topright");
 
 com.taboca.upvisor.registerWidget( ".container" , "org/fisl/noticias-fisl/noticias.js","fislnews", "area_panel3");
 //com.taboca.upvisor.registerWidget( ".container" , "org/fisl/typing2/typing.js","fislnews", "area_panel3");
