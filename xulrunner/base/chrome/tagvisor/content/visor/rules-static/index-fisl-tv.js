0400ea0a302dea7aca9dd3b1eef0161f2460f6eb
Marcio Galli <marciogalli@taboca.local>
2010-07-20 00:54:08 -0700

    Rules tv video

--- xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-fisl-tv.js
+++ xulrunner/base/chrome/tagvisor/content/visor/rules-static/index-fisl-tv.js
@@ -1,9 +1,9 @@
 com.taboca.upvisor.registerWidget( null , "org/fisl/layout-tv/gfxless.js","container");
 com.taboca.upvisor.registerWidget( null , "org/fisl/calendar/calendar-video.js","calendar");
-com.taboca.upvisor.registerWidget( ".container" , "org/fisl/twitter-eventos-side/twitter.js","clock", "area_bottomright");
+com.taboca.upvisor.registerWidget( ".container" , "org/fisl/twitter-eventos-side/twitter.js","clock", "area_topright");
 com.taboca.upvisor.registerWidget( ".container" , "org/fisl/twitter-eventos/twitter.js","twitterslash", "area_panel2");
-com.taboca.upvisor.registerWidget( ".container" , "org/fisl/typing/typing.js","twitter", "area_panel3");
-com.taboca.upvisor.registerWidget( ".container" , "org/fisl/videoexample/example.js","uspmain", "area_bottom");
+com.taboca.upvisor.registerWidget( ".container" , "org/fisl/typing2/typing.js","twitter", "area_panel3");
+com.taboca.upvisor.registerWidget( ".container" , "org/fisl/videoexample/example.js","video", "area_topleft");
 com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".calendar", "start"); } );
 
 
