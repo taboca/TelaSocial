29360dece0c203102149f48133dc124feeb609df
Marcio Galli <marciogalli@taboca.local>
2010-07-29 10:05:50 -0700

    grade in

--- xulrunner/base/chrome/tagvisor/content/visor/com/icmc/grid-sing/grade.js
+++ xulrunner/base/chrome/tagvisor/content/visor/com/icmc/grid-sing/grade.js
@@ -31,7 +31,7 @@ var grade =  {
 			var innerTd = "<tr id='rule_"+hours+"' ><td align='right'>"+eventItem.inicio+" - "+ eventItem.fim+" </td><td>"+ eventItem.descricao +"<span class='gradeLocal'> @ "+eventItem.local+"</span></tr>";
 			innerAll += innerTd;
 		}
-		this.element.innerHTML+="<table class='gradeTable'  border='0' cellpadding='10'>"+head+innerAll+"</table>";
+		this.element.innerHTML+="<table class='gradeTable'   border='0' cellpadding='10'>"+head+innerAll+"</table>";
 		this.tick();
 
 	} ,
@@ -80,6 +80,7 @@ var grade =  {
 
 		.gradeTable td { 
 
+			border:0px;
 			border-bottom:1px solid #ddd; 
 		} 
 
@@ -113,6 +114,9 @@ var grade =  {
 this.descricao["28"] = "11th International Workshop on Real and Complex Singularities";
 this.local["28"] = "Wednesday 28th @ ICMC USP";
 
+this.descricao["29"] = "11th International Workshop on Real and Complex Singularities";
+this.local["29"] = "Thursday 29th @ ICMC USP";
+
 	} ,
 
 evento : { 
@@ -125,7 +129,27 @@ evento : {
 { inicio: "12:00 ",fim: "13:00",descricao: 'Singularities in the diﬀerential equations for the motion underfocal attraction and radial drift in rotating medium - Jorge Sotomayor',sigla: "  ",local: "Room [4-001]  ",apresentador:""},
 { inicio: "13:00 ",fim: "- ",descricao: 'Enjoy the BBQ Party ',sigla: "  ",local: "",apresentador:""}
 
-]
+], 
+"29": [ 
+
+{ inicio: "8:10 ", fim: "9:00 ", descricao: "Mini-course J. Nuno", sigla: "- ",local: "Auditorium ",apresentador: ""},
+{ inicio: "9:00 ", fim: "10:00 ", descricao: "Koszul free divisors and relatives - Luis Narváez Macarro", sigla: "- ",local: "Auditorium",apresentador: ""},
+{ inicio: "10:00", fim: "11:00 ", descricao: "Noncommutative Desingularization - A Survey - Ragnar-Olaf Buchweitz", sigla: "- ",local: "Auditorium",apresentador: ""},
+{ inicio: "11:00 ",fim: "11:20",descricao: 'Delightful coffee break',sigla: "  ",local: "Open Library's Patio",apresentador:""},
+{ inicio: "11:20 ",fim: "12:00",descricao: 'Suﬃciency of jets from the plane to the plane - Hans Brodersen [4-001]; Families of curve congruences on Lorentzian surfaces and pencils of quadratic forms - Ana Claudia Nabarro [4-003]; The Lo jasiewicz exponent of a set of ideals and applications - Carles Bivia [4-005]',sigla: "  ",local: "[4-001], [4-003], [4-005]",apresentador:""},
+{ inicio: "12:00 ",fim: "12:20",descricao: 'A Short Exact Sequence for Multiple Point Spaces - Ayse Altintas [4-001]; New Open-Book Decompositions in Singularity Theory - Haydée Aguilar-Cabrera [4-003] ',sigla: "  ",local: "[4-001], [4-003]",apresentador:""},
+{ inicio: "12:20 ",fim: "14:00",descricao: 'Lunch',sigla: "  ",local: "",apresentador:""},
+{ inicio: "14:00 ",fim: "14:40",descricao: 'On metric embedding of complex algebraic sets - Alexandre Fernandes [4-001]; Bitengencies on Higher Dimensional Immersed Manifolds - Daniel Dreibelbis [4-003]; Vector fields liftable over finitely determined multigerms of corank at most one - Takashi Nishimura [4-005]  ',sigla: "  ",local: "[4-001], [4-003], [4-005]",apresentador:""},
+{ inicio: "14:40 ",fim: "15:20",descricao: 'Properties of free divisors of linear type using theory of D-modules - Francisco Calderón [4-001]; Extensions, generalizations and results related to the Four Vertex Theorem - Sueli I R Costa [4-003]; Geometry of the space of treelike shapes - Aasa Feragen [4-005]',sigla: "  ",local: "[4-001], [4-003], [4-005]",apresentador:""},
+{ inicio: "15:20 ",fim: "15:40",descricao: 'Poster Session',sigla: "  ",local: "Library's open patio",apresentador:""},
+{ inicio: "15:40 ",fim: "16:00",descricao: 'Coffee Break',sigla: "  ",local: "Library's open patio",apresentador:""},
+{ inicio: "16:00", fim: "17:00 ", descricao: "Symplectic Singularities - Duco van Straten", sigla: "- ",local: "Auditorium",apresentador: ""},
+{ inicio: "17:00", fim: "18:00 ", descricao: "David’s Mathematics - Victor V Goryunov ", sigla: "- ",local: "Auditorium",apresentador: ""},
+{ inicio: "18:00", fim: "19:30 ", descricao: " ", sigla: "- ",local: "",apresentador: ""},
+{ inicio: "19:30 ",fim: "- ",descricao: 'Dinner event party',sigla: "  ",local: "",apresentador:""}
+
+] 
+
 
 }
 
