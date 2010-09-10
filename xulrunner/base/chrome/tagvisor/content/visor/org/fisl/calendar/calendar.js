0b6d4f78882ce6fb133b95e345caf6a909461e19
Marcio Galli <marciogalli@taboca.local>
2010-09-09 18:48:33 -0700

    Rules file to IFSC

---
+++ xulrunner/base/chrome/tagvisor/content/visor/org/fisl/calendar/calendar.js
@@ -0,0 +1,39 @@
+
+c = require("choreographer");
+timer = require("timer");
+
+var calendar_Widget =  {
+
+	name   : __appName,
+        target : __targetName,
+
+	start : function () { 
+
+	        this._raiseEvent(".container", "start");
+                this._raiseEvent(".container.clock", "start");
+                this._raiseEvent(".container.twitter", "start");
+                this._raiseEvent(".container.typing", "start");
+                this._raiseEvent(".container.fotos", "start");
+                this._raiseEvent(".container.twitterslash", "start");
+
+                var self = this;
+                timer.setTimeout( function(){self.kickMore()},1000);
+
+		
+	} ,
+
+	init : function () {
+		
+
+	} ,
+	kickState : false, 
+
+        kickMore: function () { 
+	       	this._raiseEvent(".container", "kick");
+              	var self = this;
+             	timer.setTimeout( function(){self.kickMore()},100);
+ 	} 
+
+}
+
+c.register(calendar_Widget);
