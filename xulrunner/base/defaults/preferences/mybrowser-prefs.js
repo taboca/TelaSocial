f6a78e2875603b589a1751ea3807e00420e4940a
Marcio Galli <marciogalli@taboca.local>
2010-09-10 20:05:35 -0700

    pref

--- xulrunner/base/defaults/preferences/mybrowser-prefs.js
+++ xulrunner/base/defaults/preferences/mybrowser-prefs.js
@@ -6,9 +6,6 @@ pref("extensions.tagvisor.auto",true);
 pref("extensions.tagvisor.zoom",".5");
 pref("extensions.tagvisor.resolution","960,560");
 
-
-
-
 pref("nglayout.debug.disable_xul_cache", true);
 pref("nglayout.debug.disable_xul_fastload", true);
 
