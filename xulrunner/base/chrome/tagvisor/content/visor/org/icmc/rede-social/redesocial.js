3157f4774fe997cb18bfe97c4640c12cefeca3a9
Rafael <rsartori@rsartori-laptop.(none)>
2010-09-23 17:05:44 -0300

    Adding more Twitter accounts and changing different colors to each account - by Akin-Net

--- xulrunner/base/chrome/tagvisor/content/visor/org/icmc/rede-social/redesocial.js
+++ xulrunner/base/chrome/tagvisor/content/visor/org/icmc/rede-social/redesocial.js
@@ -1,3 +1,40 @@
+/* ***** BEGIN LICENSE BLOCK *****
+* Version: MPL 1.1/GPL 2.0/LGPL 2.1
+*
+* The contents of this file are subject to the Mozilla Public License Version
+* 1.1 (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+*
+* Software distributed under the License is distributed on an "AS IS" basis,
+* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+* for the specific language governing rights and limitations under the
+* License.
+*
+* The Original Code is TelaSocial
+*
+* The Initial Developer of the Original Code is Taboca TelaSocial.
+* Portions created by the Initial Developer are Copyright (C) 2010
+* the Initial Developer. All Rights Reserved.
+*
+* Contributor(s):
+* Marcio Galli <mgalli@taboca.com>
+* Alexandre Koji Imai Negrão <akin@grad.icmc.usp.br>
+*
+* Alternatively, the contents of this file may be used under the terms of
+* either the GNU General Public License Version 2 or later (the "GPL"), or
+* the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+* in which case the provisions of the GPL or the LGPL are applicable instead
+* of those above. If you wish to allow use of your version of this file only
+* under the terms of either the GPL or the LGPL, and not to allow others to
+* use your version of this file under the terms of the MPL, indicate your
+* decision by deleting the provisions above and replace them with the notice
+* and other provisions required by the GPL or the LGPL. If you do not delete
+* the provisions above, a recipient may use your version of this file under
+* the terms of any one of the MPL, the GPL or the LGPL.
+*
+* ***** END LICENSE BLOCK ***** */
+
 c     = require("choreographer");
 timer = require("timer");
 
@@ -9,27 +46,53 @@ var redesocial =  {
 	crop    : "50",
 	title   : "Twitter 10",
 	feedURL : "http://twitter.com/statuses/user_timeline/75017337.rss",
+	feedURL2: "http://twitter.com/statuses/user_timeline/135019176.rss",
+	feedURL3: "http://twitter.com/statuses/user_timeline/133252196.rss",
 	feed    : null, 
+	feed2	: null,
+	feed3	: null,
 
 	style : <><![CDATA[
-		
 
-		.tweetauthor2 { 
-			color:gray; 
-			font-size:15px;
-		} 
-
-		.postitpanel {                        
+		.postitpanel1 {                        
 			color:black;                        
 			display:inline-block;
-                        background-color:#ff5;
-                        font-size:22px;
+                        background-color:#FFFFD0;
+                        font-size:18px;
 			overflow:hidden;
                         font-weight:bold;                        
                         margin:15px;
-                        padding:15px;
+                        padding:18px;
                         width:300px;
-                        height:220px;
+                        height:170px;
+			-moz-box-shadow: black 10px 10px 10px;
+                }
+
+		.postitpanel2 {                        
+			color:black;                        
+			display:inline-block;
+                        background-color:#FFFF8A;
+                        font-size:18px;
+			overflow:hidden;
+                        font-weight:bold;                        
+                        margin:15px;
+                        padding:18px;
+                        width:300px;
+                        height:170px;
+			-moz-box-shadow: black 10px 10px 10px;
+                }
+
+		.postitpanel3 {                        
+			color:black;                        
+			display:inline-block;
+                        background-color:#FFFF45;
+                        font-size:18px;
+			overflow:hidden;
+                        font-weight:bold;                  
+                        margin:15px;
+                        padding:18px;
+                        width:300px;
+                        height:170px;
 			-moz-box-shadow: black 10px 10px 10px;
                 }
 
@@ -79,26 +142,28 @@ var redesocial =  {
 
 		this.feed = new this._service_google.feeds.Feed(this.feedURL);
 		this.feed.setResultFormat(this._service_google.feeds.Feed.XML_FORMAT);
-		this.feed.setNumEntries(10);
+		this.feed.setNumEntries(5);
 
+		this.feed2 = new this._service_google.feeds.Feed(this.feedURL2);
+		this.feed2.setResultFormat(this._service_google.feeds.Feed.XML_FORMAT);
+		this.feed2.setNumEntries(5);
 
-
+		this.feed3 = new this._service_google.feeds.Feed(this.feedURL3);
+		this.feed3.setResultFormat(this._service_google.feeds.Feed.XML_FORMAT);
+		this.feed3.setNumEntries(5);
 	} ,
 	popTweet : function() {
 		if (this.tweetQueue.length == 0) return false;
 		var obj = this.tweetQueue.pop();
 		var t = obj.content; 
-		var c = obj.command; 
+		var c = obj.color; 
 		
 		if (t in this.tweetRepeated) {
 			return;
 		}
 		this.tweetRepeated[t] = true;
 		var k = this._coreDoc.createElement('span');
-		k.className = 'postitpanel';
-		if(c=="command") { 
-			k.className = 'command';
-		} 
+		k.className = 'postitpanel'+ c;
 		k.innerHTML = t;
 		this.element.insertBefore(k, this.element.firstChild);
 		return true;
@@ -107,45 +172,27 @@ var redesocial =  {
 	updateFeed : function() {
 		if (! this.popTweet()) {
 			var self =this; 
-			this.feed.load( function (e) {  self.__feedUpdated(e) } );
+			this.feed.load( function (e) {  self.__feedUpdated(e, 1) } );
+			this.feed2.load( function (e) {  self.__feedUpdated(e, 2) } );
+			this.feed3.load( function (e) {  self.__feedUpdated(e, 3) } );
 		}
 		var self = this;
-		timer.setTimeout( function(){self.updateFeed()},60000);
+
+		timer.setTimeout( function(){self.updateFeed()},6000);
 	},
 
-	__feedUpdated : function(result) {
-		var i;
-		if (result.error) {
-			return;
-		}
-		var items = result.xmlDocument.getElementsByTagName("item");
+	__feedUpdated : function(result, color) {
+	if (result.error) return false;
+	var items = result.xmlDocument.getElementsByTagName("item");
+		for (var i = 0; i < items.length; i++) {
+      		var titleElement = items[i].getElementsByTagName("title")[0];
+      		var title = titleElement.firstChild.nodeValue;
 
-                for (var i = 0; i < items.length; i++) {
-
-		//for (i = 0; i < result.feed.entries.length; i++) {
-      var titleElement = items[i].getElementsByTagName("title")[0];
-      var title = titleElement.firstChild.nodeValue;
-
-      //var authorElement = items[i].getElementsByTagName("author")[0];
-      //var author = authorElement.firstChild.nodeValue;
-
-      //var imgElement = items[i].getElementsByTagNameNS("http://search.yahoo.com/mrss/","content")[0];
-     // var img = imgElement.getAttribute("url");
-
-	var cmd ='';
-
-			//if (result.feed.entries[i]) {
-				//this.tweetQueue.push(  result.feed.entries[i].title + ' <span class="tweetauthor2">(by @ ' + result.feed.entries[i].author.replace(/ \(.*$/,'') + ')</span>');
-				this.tweetQueue.push( { content: '<span class="tweetauthor2">Twitter @BibliotecaICMC</span> ' + title, command: cmd } );
-			//}
-		//}
+			this.tweetQueue.push( { content: title, color: color } );
 		}
 
-		//this.lastid = result.feed.entries[i-1].link.match(/\d+$/);
-		//this.feed = new this._service_google.feeds.Feed(this.feedURL + '&since_id=' + this.lastid);
-		//this.feed.setNumEntries(10);
 	}
-
 }
+
 
 c.register(redesocial);
