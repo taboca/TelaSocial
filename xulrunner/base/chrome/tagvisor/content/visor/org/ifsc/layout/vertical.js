38240d6554c7afcf13ef7f14eabdcbc55566d6d7
Marcio Galli <taboca@gmail.com>
2010-11-17 15:15:10 -0200

    tabs layout will be dynamic - support to multi tabs

--- xulrunner/base/chrome/tagvisor/content/visor/org/ifsc/layout/vertical.js
+++ xulrunner/base/chrome/tagvisor/content/visor/org/ifsc/layout/vertical.js
@@ -9,6 +9,16 @@ var layoutVertical =  {
         targetId : __targetId,
         _coreDoc: null,
 
+        tabsColors: ['background-color:rgb(253,181,37);','background-color:rgb(100,197,210);','background-color:rgb(14,148,171)'],
+        tabsPanelColors: ['background-color:rgb(253,181,37);','background-color:rgb(100,197,210);','background-color:rgb(14,148,171);'],
+        tabsIds: ['tab1','tab2','tab3'],
+        tabsTitles: ['Acontece','Fotos','Defesas'],
+        tabsPanelId: ['area_panel1','area_panel2','area_panel3'],
+
+	tabsTotal: 0,
+        movepos : 0,
+        moveWidth: 1080, 
+
 	style : <><![CDATA[
 
 	html { padding:0; margin:0; overflow:hidden; font-family: verdana, arial, sans-serif } 
@@ -27,30 +37,23 @@ var layoutVertical =  {
 	} 
 
 	.panel {
-		width:1060px;
 		margin-left:10px;
-	}
+		display:none;
+		width:1060px;
+		height:1000px;
+		overflow:hidden;
+	} 
 
 	.tab {
                 font-size:28px;
-		padding:.5em;
+		padding:10px;
+		margin:0px;
                 font-weight:bold;
         }
-
-	#tab1, #tab2, #tab3 { 
-		z-index:1000;
-	}
-
-	#area_bottomright { 
-	} 
 
 	#area_topright { 
 		padding-top:90px;
 	} 
-
-        .cor_tab1 { background-color:rgb(253,181,37); }
-        .cor_tab2 { background-color:rgb(100,197,210); }
-        .cor_tab3 { background-color:rgb(14,148,171);  }
 
         .transp { background-color:transparent; }
 
@@ -70,12 +73,6 @@ var layoutVertical =  {
 		padding:0;
 	}
 
-	.panel { 
-		display:none;
-		height:1000px;
-		overflow:hidden;
-	} 
-
 	#area_bottom {
 		padding:1em;
                 height:270px;
@@ -86,11 +83,6 @@ var layoutVertical =  {
 		font-weight:bold;
         }
 	
-	#area_panel1 span { 
-		margin-left:15px;
-		margin-top:10px;
-	} 
-
 	#area_midmid { 
 		width:1080px; 
 		height:420px; 
@@ -99,55 +91,19 @@ var layoutVertical =  {
 
 
 ]]></>,
-
-
-        movepos : 0,
-        moveleft: true,
-	
         kick: function () {
 		this.movepos+=2;
                 this._coreDoc.getElementById("pointer").style.left=this.movepos+"px";
-		if(this.movepos > 700 && this.movepos < 1080 ) { 
-                        this._coreDoc.getElementById("tab1").className="cor_tab1";
-                        this._coreDoc.getElementById("tab2").className="cor_tab2";
-                        this._coreDoc.getElementById("tab3").className="transp";
-			this._coreDoc.body.style.backgroundColor="rgb(14,148,171)";
-                        this._coreDoc.getElementById("area_panel3").style.display="block";
-                        this._coreDoc.getElementById("area_panel2").style.display="none";
-                        this._coreDoc.getElementById("area_panel1").style.display="none";
-		} 
-		if(this.movepos > 350 && this.movepos < 700) { 
-                        this._coreDoc.getElementById("tab1").className="cor_tab1";
-                        this._coreDoc.getElementById("tab2").className="transp";
-                        this._coreDoc.getElementById("tab3").className="cor_tab3";
-			this._coreDoc.body.style.backgroundColor="rgb(100,197,210)";
-                        this._coreDoc.getElementById("area_panel2").style.display="block";
-                        this._coreDoc.getElementById("area_panel1").style.display="none";
-                        this._coreDoc.getElementById("area_panel3").style.display="none";
-		} 
-		if(this.movepos > 0  && this.movepos < 350) { 
-                        this._coreDoc.getElementById("tab1").className="transp";
-                        this._coreDoc.getElementById("tab2").className="cor_tab2";
-                        this._coreDoc.getElementById("tab3").className="cor_tab3";
-			this._coreDoc.body.style.backgroundColor="rgb(253,181,37)";
-			//this._coreDoc.body.style.backgroundColor="rgb(255,255,255)";
-                        this._coreDoc.getElementById("area_panel1").style.display="block";
-                        this._coreDoc.getElementById("area_panel2").style.display="none";
-                        this._coreDoc.getElementById("area_panel3").style.display="none";
-		} 
-		if(this.movepos > 1080 ) { 
-
-			this.movepos = 0
-
-		} 
+		var currIndex = Math.floor(this.movepos/this.tabsWidth); 
+		this._service_jquery(".panel",this._coreDoc).attr("style","display:none");
+                this._coreDoc.getElementById(this.tabsPanelId[currIndex]).setAttribute("style","display:block");
+                this._coreDoc.getElementById("panelContainer").setAttribute("style",this.tabsPanelColors[currIndex]);
+		if(this.movepos > 1080 ) this.movepos = 0;
         },
 
 	start : function () { 
-
 		var importedElement = this._coreDoc.createElement("div");
-
 		importedElement.innerHTML =  <>
-
 <div id="frame">
 <div id='pointer'>
 </div>
@@ -170,31 +126,10 @@ var layoutVertical =  {
 
 </td>
 </tr>
-<tr style="height:50px">
-<td class='cor_tab1' id='tab1'>
-<div class='tab'>
-Acontece 
-</div>
-</td>
-<td class='cor_tab2' id='tab2'>
-<div class='tab'>
-Fotos 
-</div>
-</td>
-<td class='cor_tab3' id='tab3'>
-<div class='tab'>
-Defesas
-</div>
-</td>
+<tr style="height:50px" id='tabContainer'>
 </tr>
 <tr>
-<td colspan="3" >
-<div class="panel" id='area_panel1' >
-</div>
-<div class="panel" id='area_panel2'>
-</div>
-<div class="panel" id='area_panel3'>
-</div>
+<td id='panelContainer'>
 </td>
 </tr>
 <tr>
@@ -212,7 +147,25 @@ Defesas
 
 		this._coreDoc.getElementById(this._getId()).appendChild(importedElement);
 
+		this.tabsWidth = parseInt(1060/this.tabsTitles.length);
+		for(var i=0;i<this.tabsTitles.length;i++) { 
+			var tdTab = this._coreDoc.createElement("td");
+			tdTab.setAttribute("style",this.tabsColors[i]+";width:"+this.tabsWidth+"px");
+			tdTab.setAttribute("id",this.tabsIds[i]);
+   			tdTab.innerHTML="<div class='tab'>"+this.tabsTitles[i]+"</div>";
+			this._coreDoc.getElementById("tabContainer").appendChild(tdTab); 
+
+			var divPanel = this._coreDoc.createElement("div");
+			divPanel.setAttribute("class","panel");
+			divPanel.setAttribute("style",this.tabsPanelColors[i]);
+			divPanel.setAttribute("id",this.tabsPanelId[i]);
+
+			this._coreDoc.getElementById("panelContainer").appendChild(divPanel); 
+		} 
+		this._coreDoc.getElementById("panelContainer").setAttribute("colspan",this.tabsTitles.length);
+
 	} ,
+
 
 	init : function () {
 	try { 
