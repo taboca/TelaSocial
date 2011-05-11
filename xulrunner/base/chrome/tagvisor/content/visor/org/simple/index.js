9879ab5e21c64ba057fa3b14fe6bd65891b9ed91
Marcio Galli <taboca@gmail.com>
2011-05-11 01:36:18 -0300

    clean

--- xulrunner/base/chrome/tagvisor/content/visor/org/simple/index.js
+++ xulrunner/base/chrome/tagvisor/content/visor/org/simple/index.js
@@ -1,11 +1,8 @@
 
-com.taboca.upvisor.registerWidget( null , "org/ifsc/layout-vertical/gfxless.js","container");
-com.taboca.upvisor.registerWidget( null , "org/ifsc/calendar/calendar.js","calendar");
-com.taboca.upvisor.registerWidget( ".container" , "org/ifsc/clock/clock.js","clock", "area_topmid");
-com.taboca.upvisor.registerWidget( ".container" , "org/ifsc/twitter-pool/twitter.js","twitter", "area_panel1");
-com.taboca.upvisor.registerWidget( ".container" , "org/ifsc/fotogrid-vertical/fotogrid.js","fotos", "area_panel2");
-com.taboca.upvisor.registerWidget( ".container" , "org/ifsc/twitter-postit/twitter.js","twitterslash", "area_panel3");
-com.taboca.upvisor.registerWidget( ".container" , "org/ifsc/typing2/typing.js","typing", "area_bottom");
+com.taboca.upvisor.registerWidget( null , "org/simple/layout/vertical.js","container");
+com.taboca.upvisor.registerWidget( null , "org/simple/calendar/calendar.js","calendar");
+com.taboca.upvisor.registerWidget( ".container" , "org/simple/clock/clock.js","clock", "area1");
+com.taboca.upvisor.registerWidget( ".container" , "org/simple/clock/clock.js","clocktwo", "area2");
 
 com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".calendar", "start"); } ); 
 
