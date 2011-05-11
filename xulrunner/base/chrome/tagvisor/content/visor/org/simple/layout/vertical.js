3698bac528a0b0e68478db0073553464095d204d
Marcio Galli <taboca@gmail.com>
2011-05-11 03:33:31 -0300

    stype also async load

--- xulrunner/base/chrome/tagvisor/content/visor/org/simple/layout/vertical.js
+++ xulrunner/base/chrome/tagvisor/content/visor/org/simple/layout/vertical.js
@@ -8,33 +8,29 @@ var layoutVertical =  {
         targetId : __targetId,
         _coreDoc: null,
 
-	style : <><![CDATA[
-
-div { 
-	border:1px solid black;
-	margin:1em;
-	padding:1em;
-} 
-
-]]></>,
-     
         asyncStart: function (data) { 
 		var importedElement = this._coreDoc.createElement("div");
 		importedElement.innerHTML =  data;
 		this._coreDoc.getElementById(this._getId()).appendChild(importedElement);
         }, 
        
-	start : function () { 
+	start:function () { 
                 var self = this;
 		c.load("./org/simple/layout/data.html", function s(d) {
 			self.asyncStart(d);
 		} , function e(i) { console.log(i) } ); 
+		c.load("./org/simple/layout/data.css", function s(d) {
+			self.asyncStyle(d);
+		} , function e(i) { console.log(i) } ); 
 	} ,
 
 	init : function () {
+	}, 
+
+	asyncStyle: function (data) { 
 	 	var style = this._coreDoc.createElementNS("http://www.w3.org/1999/xhtml", "style");
 		this._coreDoc.getElementById("headtarget").appendChild(style);
-		style.innerHTML=this.style; 
+		style.innerHTML=data; 
 	}
 
 }
