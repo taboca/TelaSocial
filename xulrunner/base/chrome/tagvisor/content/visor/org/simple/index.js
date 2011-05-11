d15a2aadbcebadd2d0e334f64f50e76bd1017c5b
Marcio Galli <taboca@gmail.com>
2011-05-11 01:37:06 -0300

    fix

--- xulrunner/base/chrome/tagvisor/content/visor/org/simple/index.js
+++ xulrunner/base/chrome/tagvisor/content/visor/org/simple/index.js
@@ -1,10 +1,10 @@
 
 com.taboca.upvisor.registerWidget( null , "org/simple/layout/vertical.js","container");
-com.taboca.upvisor.registerWidget( null , "org/simple/calendar/calendar.js","calendar");
 com.taboca.upvisor.registerWidget( ".container" , "org/simple/clock/clock.js","clock", "area1");
 com.taboca.upvisor.registerWidget( ".container" , "org/simple/clock/clock.js","clocktwo", "area2");
 
-com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".calendar", "start"); } ); 
+com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".clock", "start"); } ); 
+com.taboca.upvisor.startEngine( function () { com.taboca.upvisor.raiseEvent(".clocktwo", "start"); } ); 
 
 
 
