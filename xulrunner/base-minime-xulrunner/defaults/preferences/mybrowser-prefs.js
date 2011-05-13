c70eb544c533075fc2eaf34897a1670448e5c65f
Marcio Galli <taboca@gmail.com>
2011-05-13 12:17:36 -0300

    Example small XULRunner app

---
+++ xulrunner/base-minime-xulrunner/defaults/preferences/mybrowser-prefs.js
@@ -0,0 +1,6 @@
+pref("toolkit.defaultChromeURI", "chrome://telasocial/content/telasocial.xul");
+pref("general.useragent.extra.telasocial", "TelaSocial CrowdBrowsing/0.1");
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
+pref("browser.dom.window.dump.enabled", true);
+
