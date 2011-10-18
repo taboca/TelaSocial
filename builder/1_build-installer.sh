33f2fde72c26d1bed3b7512b0e70da8588e1fd71
Marcio Galli <taboca@gmail.com>
2011-10-18 14:33:36 -0200

    fix build

--- builder/1_build-installer.sh
+++ builder/1_build-installer.sh
@@ -1,23 +1,23 @@
 rm -rf ./telasocial-output.zip
 rm -rf ./installer
 rm -rf TelaSocial
-git clone git://github.com/taboca/TelaSocial.git
+#git clone git://github.com/taboca/TelaSocial.git
 mkdir ./installer
 echo .
-cp -r ./TelaSocial/linux/ubuntu-install/* ./installer
+cp -r ../linux/ubuntu-install/* ./installer
 echo .
 mkdir ./installer/build
-cp -r ./TelaSocial/xulrunner/base/* ./installer/build
+cp -r ../xulrunner/base/* ./installer/build
 rm -rf  ./installer/build/chrome/tagvisor/content/visor/*
 cp index.html ./installer/build/chrome/tagvisor/content/visor/
 
 # local
-cp -r ../apps/ifsc-usp-v2/* ./installer/build/chrome/tagvisor/content/visor
+#cp -r ../apps/ifsc-usp-v2/* ./installer/build/chrome/tagvisor/content/visor
 
 cp mybrowser-prefs.js ./installer/build/defaults/preferences/mybrowser-prefs.js
 #cp xorg.conf ./installer/screen/ubuntu/xorg.conf
 cp -r ./build/xulrunner ./installer/build
 cp -r ./installer/build/xulrunner/xulrunner-stub ./installer/build/telasocial
-zip -r telasocial-ifsc.zip ./installer
-echo telasocial-output.zip is your build
+zip -r telasocial.zip ./installer
+echo telasocial.zip is your build
 
