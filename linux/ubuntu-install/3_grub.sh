2a9c3dbc7e7b3a22f53c0c6a1da74b24f83ace66
Marcio Galli <taboca@gmail.com>
2012-06-25 11:56:31 -0300

    only backup one time

--- linux/ubuntu-install/3_grub.sh
+++ linux/ubuntu-install/3_grub.sh
@@ -1,5 +1,7 @@
-mkdir -p backup/etc/default/
-cp /etc/default/grub ./backup/etc/default/
+if [ ! -d "./backup" ]; then
+   mkdir -p backup/etc/default/
+   cp /etc/default/grub ./backup/etc/default/
+fi 
 cat ./backup/etc/default/grub | sed s/GRUB_CMDLINE_LINUX_DEFAULT.*/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet text\"/ > /etc/default/grub
 update-grub
 
