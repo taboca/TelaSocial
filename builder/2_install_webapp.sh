b177f1e31923d4b65554941fc431dc030605b7c9
Marcio Galli <taboca@gmail.com>
2012-06-25 10:35:09 -0300

    Notes about current example app

--- builder/2_install_webapp.sh
+++ builder/2_install_webapp.sh
@@ -1,8 +1,9 @@
-echo "Override index.html..."
-cp index.html ./installer/build/chrome/tagvisor/content/visor/
 
 echo "Copy YOUR app..."
 cp -r ../apps/base/* ./installer/build/chrome/tagvisor/content/visor
+
+echo "Override index.html... PS: our index.html calls index2.html from the web app"
+cp index.html ./installer/build/chrome/tagvisor/content/visor/
 
 echo "Add YOUR prefs..."
 cp mybrowser-prefs.js ./installer/build/defaults/preferences/mybrowser-prefs.js
