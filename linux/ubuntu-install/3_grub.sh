76677ac4be8cb3a6bf711f73bf590199bd75410f
Marcio Galli <taboca@gmail.com>
2012-06-25 12:13:55 -0300

    fix regexp

--- linux/ubuntu-install/3_grub.sh
+++ linux/ubuntu-install/3_grub.sh
@@ -2,7 +2,7 @@ if [ ! -d "./backup" ]; then
    mkdir -p backup/etc/default/
    cp /etc/default/grub ./backup/etc/default/
 fi 
-cat ./backup/etc/default/grub | sed s/quiet.*\"/quiet text\"/ > /etc/default/grub
+cat ./backup/etc/default/grub | sed s/quiet.*\"/quiet\ text\"/ > /etc/default/grub
 
 update-grub
 
