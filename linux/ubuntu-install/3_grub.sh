625526475292ce18af18573ac5b864efd3916cb3
Marcio Galli <taboca@gmail.com>
2012-06-25 11:52:12 -0300

    grub

--- linux/ubuntu-install/3_grub.sh
+++ linux/ubuntu-install/3_grub.sh
@@ -1,6 +1,6 @@
 mkdir -p backup/etc/default/
 cp /etc/default/grub ./backup/etc/default/
-cat ./backup/etc/default/grub | sed s/splash/splash\ text/ > /etc/default/grub
+cat ./backup/etc/default/grub | sed s/GRUB_CMDLINE_LINUX_DEFAULT.*/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet text\"/ > /etc/default/grub
 update-grub
 
 
