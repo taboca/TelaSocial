afce08ecd5080e1da260a7a75f51e19780837e77
Marcio Galli <taboca@gmail.com>
2012-09-05 17:13:43 -0300

    Cleaner widgets for 'abas' and 1080 layout

--- apps/base/index-1080-1920.js
+++ apps/base/index-1080-1920.js
@@ -3,27 +3,14 @@
 $(document).ready(function() {
 
    /* This needs to grows to become the grid system placement */
-
    /* The exercise here is that we want to load the sub elements and internal 
       iframe pages are ready. */
 
    register("/main/footer", "cabecalho", "./header/index.html", iframeTemplate);
-//   We failed miserably in the attempt to add the clock to the actual inner 
-//   div in the iframe. This because we do not know the rule within the Iframe 
-//   And it is so particular. A workaround would be the do path1,path2 but 
-//   would that make sense? Why would we specifly precisely inner iframes to be 
-//   loaded at the actual component level ( first layer ). We need to organize 
-//   a bit better how the composition actually works here and this whole  
-//   child -> iframe models..
-//   register("/main/footer/cabecalho", "clock", "./tempo/index.html", iframeTemplate);
-
    register("/main/lado", "sidebar", "./twitter/index.html", iframeTemplate);
-   //register("/main/middle", "acontece", "./acontece/index.html", iframeTemplate);
-  // register("/main/middle", "acontece", "./acontece-middle/index.html", iframeTemplate);
    register("/main/middle", "meio", "./abas/index.html", iframeTemplate);
    register("/main/topmiddle", "mid", "./destaques/index.html", iframeTemplate);
    register("/main/bottomsection", "grade", "./grade-ical/index.html", iframeTemplate);
-   //register("/main/bottomsection", "grade", "./galeria-bottom/index.html", iframeTemplate);
    compile();   
    setTimeout('startEngine()',5000);
 
