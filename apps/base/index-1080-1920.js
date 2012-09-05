8324ccabaaaf7c08783a0a3286f47c511f2eea33
Marcio Galli <taboca@gmail.com>
2012-09-05 17:20:33 -0300

    Index 1080 to index twitter-340x900

--- apps/base/index-1080-1920.js
+++ apps/base/index-1080-1920.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       iframe pages are ready. */
 
    register("/main/footer", "cabecalho", "./header/index.html", iframeTemplate);
-   register("/main/lado", "sidebar", "./twitter/index.html", iframeTemplate);
+   register("/main/lado", "sidebar", "./twitter/index-340-900.html", iframeTemplate);
    register("/main/middle", "meio", "./abas/index.html", iframeTemplate);
    register("/main/topmiddle", "mid", "./destaques/index.html", iframeTemplate);
    register("/main/bottomsection", "grade", "./grade-ical/index.html", iframeTemplate);
