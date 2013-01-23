a272ea055f9cc6764450c5922fe38d5157db2fe9
marcio <mgalli@telasocial.com>
2013-01-23 17:33:39 -0200

    No app

--- builder/2_install_webapp.sh
+++ builder/2_install_webapp.sh
@@ -1,6 +1,6 @@
 
 echo "Copy YOUR app..."
-cp -r ../apps/base/* ./installer/build/chrome/tagvisor/content/visor
+#cp -r ../apps/base/* ./installer/build/chrome/tagvisor/content/visor
 
 echo "Override index.html... PS: our index.html calls index2.html from the web app"
 cp index.html ./installer/build/chrome/tagvisor/content/visor/
