8c006d6eede15567091cd6d270c7addde2279a25
marcio <taboca@gmail.com>
2013-06-21 20:34:32 -0300

    pref clean

--- xulrunner/base/defaults/preferences/mybrowser-prefs.js
+++ xulrunner/base/defaults/preferences/mybrowser-prefs.js
@@ -1,11 +1,10 @@
 pref("toolkit.defaultChromeURI", "chrome://telasocial/content/telasocial.xul");
-pref("extensions.tagvisor.app","org/icmc/index-icmc-4.0.js");
 pref("general.useragent.extra.telasocial", "TelaSocial CrowdBrowsing/0.1");
 pref("extensions.tagvisor.auto",true);
 
 // These are debug mode you can simulate any type of TV with the zoom out 
-pref("extensions.tagvisor.zoom",".5");
-pref("extensions.tagvisor.resolution","960,560");
+//pref("extensions.tagvisor.zoom",".5");
+//pref("extensions.tagvisor.resolution","960,560");
 
 pref("nglayout.debug.disable_xul_cache", true);
 pref("nglayout.debug.disable_xul_fastload", true);
