8a6bb95c41e0ce041b63b67809bdc643fe41b8e3
marcio <taboca@gmail.com>
2013-06-21 20:35:31 -0300

    Pref defaults

--- builder/2_install_webapp.sh
+++ builder/2_install_webapp.sh
@@ -5,7 +5,7 @@ echo "Copy YOUR app..."
 echo "Override index.html... PS: our index.html calls index2.html from the web app"
 cp index.html ./installer/build/chrome/tagvisor/content/visor/
 
-echo "Add YOUR prefs..."
-cp mybrowser-prefs.js ./installer/build/defaults/preferences/mybrowser-prefs.js
+#echo "Add YOUR prefs..."
+#cp mybrowser-prefs.js ./installer/build/defaults/preferences/mybrowser-prefs.js
 
 echo "*** Build directory installed - you can zip or install from ./installer ***"
